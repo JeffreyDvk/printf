9c3947767eeaf4af0a0c2fd798eca3745f0e6335
JeffreyDvk <jeffrey.dvk.fr@gmail.com>
2022-04-19 09:24:05 +0100

    Init printf

--- print_hex.c
+++ print_hex.c
@@ -1,43 +1,69 @@
 #include "main.h"
-/**
- * print_hex - prints a decimal in hexadecimal
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
- */
-int print_hex(va_list arguments, char *buf, unsigned int ibuf)
-{
-	int int_input, i, isnegative, count, first_digit;
-	char *hexadecimal, *binary;
 
-	int_input = va_arg(arguments, int);
-	isnegative = 0;
-	if (int_input == 0)
+/**
+ * treat_flags_hex - treat flags
+ * @flags: string of flags
+ * @buffer: to store resulr
+ * @size: size of buffer
+ * @_case: case of characters
+ *
+ */
+void treat_flags_hex(char *flags, char *buffer, int *size, char _case)
+{
+	int i;
+
+	for (i = 0; flags && flags[i]; i++)
 	{
-		ibuf = handl_buf(buf, '0', ibuf);
-		return (1);
-	}
-	if (int_input < 0)
-	{
-		int_input = (int_input * -1) - 1;
-		isnegative = 1;
-	}
-	binary = malloc(sizeof(char) * (32 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 32);
-	hexadecimal = malloc(sizeof(char) * (8 + 1));
-	hexadecimal = fill_hex_array(binary, hexadecimal, 0, 8);
-	for (first_digit = i = count = 0; hexadecimal[i]; i++)
-	{
-		if (hexadecimal[i] != '0' && first_digit == 0)
-			first_digit = 1;
-		if (first_digit)
+		if (flags[i] == '#')
 		{
-			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
-			count++;
+			buffer[(*size)++] = _case + 23;
+			buffer[(*size)++] = '0';
 		}
 	}
-	free(binary);
-	free(hexadecimal);
-	return (count);
+}
+/**
+ * print_hex - print usigned decimal as hexadecimal
+ * @modif: struct modifier containig modifier fields
+ * @ap: va_list pointer containig unsigned int to print
+ *
+ * Return: number of printed characters
+ */
+
+char *print_hex(modifier_t *modif, va_list ap)
+{
+	unsigned int n, aux;
+	int i = 0, j = 0;
+	char buffer[11], _case, *res_str;
+
+	if (!ap || !modif)
+		return (0);
+	n = va_arg(ap, unsigned int);
+	_case = modif->specifier == 'x' ? 'a' : 'A';
+	if (n == 0)
+	{
+		j = 1;
+		res_str = malloc(sizeof(char) * 2);
+		res_str[0] = '0';
+	}
+	else
+	{
+		while (n)
+		{
+			aux = n % 16;
+			if (aux > 9)
+				buffer[i++] = (aux % 10) + _case;
+			else
+				buffer[i++] = aux + '0';
+			n = n / 16;
+		}
+		treat_flags_hex(modif->flags, buffer, &i, _case);
+		res_str = malloc(sizeof(char) * i);
+		i--;
+		while (i >= 0)
+		{
+			res_str[j++] = buffer[i--];
+		}
+	}
+	res_str[j] = '\0';
+	return (res_str);
 }
